3fb0033d7755d643613ac7855d7d109774ce1276
Tilo-K <tiloklarenbeek@gmail.com>
2023-10-23 19:47:57 +0200

    Fixed creating and deleting

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 use clap::{Parser, Subcommand};
+use rpassword::read_password;
 use std::fs;
 use std::io;
+use std::io::Write;
+use std::path::PathBuf;
 use std::sync::Mutex;
-
 mod pastebin;
 
 static API_KEY: Mutex<String> = Mutex::new(String::new());
+static USER_KEY: Mutex<String> = Mutex::new(String::new());
 
 fn get_api_key() {
     let home_dir = match home::home_dir() {
@@ -22,7 +25,9 @@ fn get_api_key() {
     if key == "" {
         let mut input = String::new();
 
-        println!("Pastebin API key: ");
+        print!("Pastebin API key: ");
+        io::stdout().lock().flush().unwrap();
+
         io::stdin()
             .read_line(&mut input)
             .expect("Error reading API_KEY");
@@ -35,6 +40,45 @@ fn get_api_key() {
     }
 
     API_KEY.lock().unwrap().push_str(&key);
+}
+
+async fn get_user_key() {
+    let home_dir = match home::home_dir() {
+        Some(path) => path,
+        None => panic!("Impossible to get your home dir!"),
+    };
+
+    let key_file = home_dir.join(".pastebin_userkey");
+    let key = match fs::read_to_string(&key_file) {
+        Ok(d) => d,
+        Err(_) => "".to_owned(),
+    };
+
+    if key == "" {
+        let mut username = String::new();
+        let mut password = String::new();
+
+        print!("Username: ");
+        io::stdout().lock().flush().unwrap();
+        io::stdin()
+            .read_line(&mut username)
+            .expect("Error reading Username");
+
+        print!("Password: ");
+        io::stdout().lock().flush().unwrap();
+        password = read_password().unwrap();
+
+        let key = pastebin::get_user_key(&API_KEY.lock().unwrap(), username, password)
+            .await
+            .unwrap();
+        USER_KEY.lock().unwrap().push_str(&key);
+
+        fs::write(key_file, key).expect("Error writing API KEY");
+
+        return;
+    }
+
+    USER_KEY.lock().unwrap().push_str(&key);
 }
 
 #[derive(Parser, Debug, Clone)]
@@ -61,15 +105,62 @@ enum Action {
 #[tokio::main]
 async fn main() {
     get_api_key();
+    get_user_key().await;
 
     let args = Cli::parse();
 
     match args.action {
-        Action::Create { file_path } => todo!(),
-        Action::Delete { paste_id } => todo!(),
+        Action::Create { file_path } => create(file_path).await,
+        Action::Delete { paste_id } => delete(paste_id).await,
         Action::Edit {
             paste_id,
             file_path,
-        } => todo!(),
-    }
+        } => edit(paste_id, file_path).await,
+    };
+}
+
+async fn create(file_path: PathBuf) {
+    let file_name = file_path.file_name().unwrap().to_str().unwrap();
+    let file_content = fs::read_to_string(&file_path).unwrap();
+
+    let paste_id = pastebin::create_paste(
+        &API_KEY.lock().unwrap(),
+        &USER_KEY.lock().unwrap(),
+        file_name.to_owned(),
+        file_content,
+        None,
+    )
+    .await
+    .unwrap();
+
+    println!("{}", paste_id);
+}
+
+async fn delete(paste_id: String) {
+    let resp = pastebin::delete_paste(
+        &API_KEY.lock().unwrap(),
+        &USER_KEY.lock().unwrap(),
+        paste_id,
+    )
+    .await
+    .unwrap();
+
+    println!("{}", resp);
+}
+
+async fn edit(paste_id: String, file_path: PathBuf) {
+    let file_name = file_path.file_name().unwrap().to_str().unwrap();
+    let file_content = fs::read_to_string(&file_path).unwrap();
+
+    let resp = pastebin::create_paste(
+        &API_KEY.lock().unwrap(),
+        &USER_KEY.lock().unwrap(),
+        file_name.to_owned(),
+        file_content,
+        Some(paste_id),
+    )
+    .await
+    .unwrap();
+
+    println!("{}", resp);
 }
--- src/pastebin.rs
+++ src/pastebin.rs
@@ -1,51 +1,88 @@
-use std::error::Error;
+use std::{collections::HashMap, error::Error};
 
 pub async fn create_paste(
     api_key: &str,
+    user_key: &str,
     name: String,
     content: String,
     paste_id: Option<String>,
 ) -> Result<String, Box<dyn Error>> {
     let client = reqwest::Client::new();
+
+    let mut map = HashMap::new();
+    map.insert("api_dev_key", api_key.trim().to_owned());
+    map.insert("api_user_key", user_key.trim().to_owned());
+    map.insert("api_option", "paste".to_owned());
+    map.insert("api_paste_code", content);
+    map.insert("api_paste_private", "0".to_owned());
+    map.insert("api_paste_expire_date", "1W".to_owned());
+
+    if paste_id.is_some() {
+        let id = paste_id.unwrap();
+        map.insert("api_paste_key", id);
+    }
+
     let mut req = client
         .post("https://pastebin.com/api/api_post.php")
-        .query(&[
-            ("api_dev_key", api_key),
-            ("api_option", "paste"),
-            ("api_paste_code", &content),
-            ("api_paste_private", "1"),
-            ("api_paste_expire_date", "1W"),
-            ("api_paste_name", &name),
-        ]);
-
-    if (paste_id.is_some()) {
-        req = req.query(&[("api_paste_key", &paste_id.unwrap())]);
-    }
+        .form(&map);
 
     let resp = req.send().await?;
     if resp.status() == 200 {
         let resp_body = resp.text().await?;
         return Ok(resp_body);
     }
-
-    return Ok(resp.status().to_string());
+    return Ok(resp.status().to_string() + "\n" + &resp.text().await?);
 }
 
-pub async fn delete_paste(api_key: &str, paste_id: String) -> Result<String, Box<dyn Error>> {
+pub async fn delete_paste(
+    api_key: &str,
+    user_key: &str,
+    paste_id: String,
+) -> Result<String, Box<dyn Error>> {
     let client = reqwest::Client::new();
+
+    let mut map = HashMap::new();
+    map.insert("api_dev_key", api_key.trim().to_owned());
+    map.insert("api_user_key", user_key.trim().to_owned());
+    map.insert("api_option", "delete".to_owned());
+    map.insert("api_paste_key", paste_id);
+
     let req = client
         .post("https://pastebin.com/api/api_post.php")
-        .query(&[
-            ("api_dev_key", api_key),
-            ("api_option", "delete"),
-            ("api_paste_key", &paste_id),
-        ])
+        .form(&map)
         .send()
         .await?;
 
     if req.status() == 200 {
         return Ok("Deleted".to_owned());
     }
+    dbg!(&req);
 
-    Ok(req.status().to_string())
+    Ok(req.status().to_string() + "\n" + &req.text().await?)
+}
+
+pub async fn get_user_key(
+    api_key: &str,
+    username: String,
+    password: String,
+) -> Result<String, Box<dyn Error>> {
+    let client = reqwest::Client::new();
+
+    let mut map = HashMap::new();
+    map.insert("api_dev_key", api_key.trim().to_owned());
+    map.insert("api_user_name", username.trim().to_owned());
+    map.insert("api_user_password", password.trim().to_owned());
+
+    let req = client
+        .post("https://pastebin.com/api/api_login.php")
+        .form(&map)
+        .send()
+        .await?;
+
+    if req.status() == 200 {
+        return Ok(req.text().await?);
+    }
+    dbg!(&req);
+    println!("{}", req.text().await?);
+    panic!("Error logging in.");
 }
